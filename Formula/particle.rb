feb2ea546f3e06fc68063149195f253045dec7e0
Matthew James <matt@MacBook-Pro-6.local>
2026-03-07 14:03:14 +0800

    Use GitHubPrivateRepositoryReleaseDownloadStrategy for private repo

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -6,20 +6,20 @@ class Particle < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-darwin-arm64"
+      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-darwin-arm64", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "860b4ff93025cc18cfa234804760bf591513c620940b169a3e94820917d5e413"
     else
-      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-darwin-amd64"
+      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-darwin-amd64", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "6327f05b0496ae62efc5a8a46bd8a1451a3b2b3671b9627f729dedfd685cf8be"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-linux-arm64"
+      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-linux-arm64", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "d90c9d509117da13b51b9191fe69225375acf53648e0e2f3861e3d17b85fe916"
     else
-      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-linux-amd64"
+      url "https://github.com/mj1618/runparticle.com/releases/download/v0.1.0/particle-linux-amd64", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "8289f3616a57dbada592053ef6e70a94d027acb08b797638928f51ff5aae439f"
     end
   end

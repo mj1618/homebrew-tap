816840d1136658a5c4f3828f6e07008fa0e56fc2
Matt James <6138817+mj1618@users.noreply.github.com>
2026-03-07 17:25:09 +0800

    Fix SHA256 hashes to match deployed v0.2.0 binaries

--- Formula/particle.rb
+++ Formula/particle.rb
@@ -7,20 +7,20 @@ class Particle < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://runparticle.com/releases/particle-darwin-arm64"
-      sha256 "7c39f59a1394af042811936c8c11681c768c66e096f29450795cc9f1f75633c0"
+      sha256 "7a9837032c89e57e3124987216979b0c285caf30df1dd27c07b6c500724c6bfe"
     else
       url "https://runparticle.com/releases/particle-darwin-amd64"
-      sha256 "a68f935d9e76a26db4fcf24d02b113a37b82f552af5ea586744f5f2cbd4b079a"
+      sha256 "6d77a942f17dc00697853ffe809dae9461dbc46296d4e34dc040243e14e20b80"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://runparticle.com/releases/particle-linux-arm64"
-      sha256 "d85ffe8277db7835f37c9bafd8529573408664ac3f26140d9fc1816528a19ff4"
+      sha256 "bc32c3b241b84a9b8a1478beb449be152c3aa040cdb111c6008d5b13b9c29ffa"
     else
       url "https://runparticle.com/releases/particle-linux-amd64"
-      sha256 "f6637bbfbff83ee7e420ae714fae675ef9bb5069ead28e71c97bb9faffbda23e"
+      sha256 "889727f557e17bef69fea2c4e492fe69cb8e04f6bd7b8de6af833fc301a909fd"
     end
   end
 
